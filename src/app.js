f89c6eb22ce0cccd54b5b8bb67aaa89dfe3570fd
YanYasimo <yanmoura142529@gmail.com>
2022-05-14 17:57:10 -0300

    CRUD on static array

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
 
 const books = [
     {id: 1, "title": "Harry Potter and the Philosopher's Stone", "author": "J.K. Rowling"},
@@ -16,5 +17,29 @@ app.get('/', (req, res) => {
 app.get('/books', (req, res) => {
     res.status(200).json(books);
 });
+
+app.post('/books', (req, res) => {
+    const book = req.body;
+    books.push(book);
+    res.status(201).json(book);
+});
+
+app.put('/books/:id', (req, res) => {
+    let book = req.body;
+    let index = findBook(req.params.id);
+    books[index].title = book.title;
+    res.status(200).json(books[index]);
+});
+
+app.delete('/books/:id', (req, res) => {
+    let {id} = req.params.id;
+    const index = findBook(id);
+    books.splice(index, 1);
+    res.send("Book deleted");
+});
+
+function findBook(id) {
+    return books.findIndex(b => b.id == id);
+};
 
 export default app;
