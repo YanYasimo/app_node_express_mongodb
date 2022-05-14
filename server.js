530883b0a63062c4332fb724bfcb432a2385cdad
YanYasimo <yanmoura142529@gmail.com>
2022-05-14 10:54:40 -0300

    Implementation Express

--- server.js
+++ server.js
@@ -1,13 +1,7 @@
-const http = require('http');
-const port = 3000;
+import app from './src/app.js';
 
-const server = http.createServer((req, res) => 
-    {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end("Hello World\n");
-    }
-);
+const port = process.env.PORT || 3000;
 
-server.listen(port, () => {
+app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`)
 });
